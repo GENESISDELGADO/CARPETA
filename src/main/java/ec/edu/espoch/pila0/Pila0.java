85733cacd26309b45019f77898c2a56039d42f1d
GENESISDELGADO <gd4820701@gmail.com>
2024-11-25 13:15:22 -0500

    carpet

---
+++ src/main/java/ec/edu/espoch/pila0/Pila0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.pila0;
+
+/**
+ *
+ * @author ACER
+ */
+public class Pila0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
